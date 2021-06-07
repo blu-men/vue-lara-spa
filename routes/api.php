a98bbae368b060010cb96c097dad4832a2c2a2b3
blu-men <cjbb5eyv@icloud.com>
2021-06-08 01:10:35 +0900

    can_delete_todotasks

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/tasks', 'TaskController@index');
 Route::post('/tasks', 'TaskController@store');
-Route::get('/tasks/{task}', 'TaskController@show');
+Route::delete('/tasks/{task}', 'TodoTaskController@destroy');
 Route::put('/tasks/{task}', 'TaskController@update');
-Route::delete('/tasks/{task}', 'TaskController@destroy');
+
 
 Route::get('/todotasks', 'TodoTaskController@index');
 Route::post('/todotasks/{task}', 'TodoTaskController@store');
