e337803656adea85e80d9863de9d67358e5b355d
blu-men <cjbb5eyv@icloud.com>
2021-06-09 12:16:44 +0900

    reverce_taskedit

--- resources/js/components/TaskEditComponent.vue
+++ resources/js/components/TaskEditComponent.vue
@@ -8,12 +8,16 @@
                         <input type="text" class="col-sm-9 form-control-plaintext" readonly id="id" v-model="task.id">
                     </div>
                     <div class="form-group row">
-                        <label for="title" class="col-sm-3 col-form-label">Project</label>
+                        <label for="title" class="col-sm-3 col-form-label">Title</label>
                         <input type="text" class="col-sm-9 form-control" id="title" v-model="task.title">
                     </div>
                     <div class="form-group row">
                         <label for="content" class="col-sm-3 col-form-label">Content</label>
                         <input type="text" class="col-sm-9 form-control" id="content" v-model="task.content">
+                    </div>
+                    <div class="form-group row">
+                        <label for="person-in-charge" class="col-sm-3 col-form-label">Person In Charge</label>
+                        <input type="text" class="col-sm-9 form-control" id="person-in-charge" v-model="task.person_in_charge">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
