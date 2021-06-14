f10bd5bcce8a5533a8219d92656d2d87216035c0
blu-men <cjbb5eyv@icloud.com>
2021-06-14 14:44:23 +0900

    make_taskTest

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,3 +26,10 @@ $factory->define(User::class, function (Faker $faker) {
         'remember_token' => Str::random(10),
     ];
 });
+
+$factory->define(App\Task::class, function (Faker $faker) {
+    return [
+        'title' => $faker->title,
+        'content' => $faker->content,
+    ];
+});
